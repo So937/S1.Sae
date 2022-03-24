da5e38669713492192fea6864799ce520022d18d
So937 <101273593+So937@users.noreply.github.com>
2022-03-24 13:22:15 +0100

    Update insert.sql

--- SAE_BD2/insert.sql
+++ SAE_BD2/insert.sql
@@ -6,4 +6,13 @@ INSERT into pays(idpays ,nom_pays ,nom_continent)
  (4,'Italie','Europe'),
  (5,'France','Europe');
 
-INSERT INTO  
+INSERT INTO cas(idcas, newcase_million, newcase, test_per_case, dates, idpays)
+ values
+ (1,,,,1),
+ (2,,,,2),
+ (3,,,,3),
+ (4,,,,4),
+ (5,,,,5);
+ 
+ INSERT INTO mort 
+ 
