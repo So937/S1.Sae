ea89c0936157a0751593f4ffd8e0f4efccd787c6
So937 <101273593+So937@users.noreply.github.com>
2022-03-24 13:11:20 +0100

    Create insert.sql

---
+++ SAE_BD2/insert.sql
@@ -0,0 +1,7 @@
+INSERT into pays(idpays ,nom_pays ,nom_continent) 
+ values 
+ (1,'Andorre','Europe'),
+ (2,'Suisse','Europe'),
+ (3,'Irlande','Europe'),
+ (4,'Italie','Europe'),
+ (5,'France','Europe');
